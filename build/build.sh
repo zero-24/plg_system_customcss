0901dffb3f03c5e9e8c16d924d740847c6c77a56
zero-24 <zero-24@users.noreply.github.com>
2020-03-23 23:32:59 +0100

    remove ready message

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,3 @@ cd ..
 if [ -f "$EXTENSION_FILENAME" ]; then rm $EXTENSION_FILENAME; fi
 zip -r $EXTENSION_FILENAME language/ customcss.php customcss.xml script.php --quiet
 sha512sum $EXTENSION_FILENAME | awk '{print $1}'
-echo 'package ready'

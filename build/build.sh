a8af2a096576063e1be108fb186f0e3fc336b54f
zero-24 <zero-24@users.noreply.github.com>
2020-03-23 23:32:11 +0100

    remove filename from the end

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,5 @@ EXTENSION_FILENAME="build/plg_system_customcss.zip"
 cd ..
 if [ -f "$EXTENSION_FILENAME" ]; then rm $EXTENSION_FILENAME; fi
 zip -r $EXTENSION_FILENAME language/ customcss.php customcss.xml script.php --quiet
-sha512sum $EXTENSION_FILENAME
+sha512sum $EXTENSION_FILENAME | awk '{print $1}'
 echo 'package ready'
